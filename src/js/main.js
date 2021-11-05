b8e30efeb85eb474fcfef76bd3d38296d00dced8
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2021-11-05 12:20:54 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,7 @@ import { settings } from './settings.js'
 markdown($('#modal-container'))
 settings()
 
-
 highlighter($('.row pre'))
-log("%cStop! better download the repository.\nhttps://github.com/MatiDragon-YT/opcode-search",
+log("Stop! better download the repository.\nhttps://github.com/MatiDragon-YT/opcode-search",
 	"color: black;background-color: #4caf50;font-size: 1rem;font-weight:bold;padding:.45rem 1rem"
 )
