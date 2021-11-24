8e480e7b87d851d37102e804d7790ffa347e31a5
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2021-11-24 17:41:52 -0300

    texts sent to .json

--- src/html/modal.js
+++ src/html/modal.js
@@ -2,45 +2,42 @@ document.write(`
 <section id="modal">
 <element id="modal-window">
 <div id="modal-header">
-	<h1 id="modal-title">Settings</h1>
+	<h1 key="t-settings" id="modal-title"></h1>
 	<span>
 		<i id="modal-save" class="icon">save</i>
 		<i id="modal-close" class="icon">close</i>
 	</span>
 </div>
 <div id="modal-container">
-	With love for you ;)
+	<span key="with"></span>
 	<br><br>
 
-	<b>Highlighter opcodes</b>
+	<b key="highl"></b>
 	<blockquote>
-		Enable at <input type="number" id="settings-limit-h">
-		<div class="info"><i class="icon">info</i><p class="comment">The highlighter by default is limited to highlighting only those opcodes that are in the lower range set here. That is, the higher the number, the more likely it is that the results will be highlighted, but it will also take longer to display the results. You can disable the functionality with 0 or it will highlight everything with -1.</p></div>
+		<span key="highe"></span> <input type="number" id="settings-limit-h">
+		<div class="info"><i class="icon">info</i><p key="highi" class="comment"></p></div>
 	</blockquote>
 	
 	<!--
-	<b>Color theme</b>
+	<b key="theme"></b>
 	<blockquote>
 		<div class="table">
-			<div class="row"><b>Element</b>
-				Nav
-				Bars
-				List
-				Scroll
+			<div class="row"><b key="themee">Element</b>
+				<span key="themees"></span>
 			</div>
-			<div class="row"><b>Color</b>
+			<div class="row"><b key="themec"></b>
 				<input type="text" disabled id="color-nav">
 				<input type="text" disabled id="color-bar">
 				<input type="text" disabled id="color-list">
 				<input type="text" disabled id="color-scroll">
 			</div>
-			<div class="row"><b>Ground</b>
+			<div class="row"><b key="themeg"></b>
 				<input type="text" disabled id="ground-nav">
 				<input type="text" disabled id="ground-bar">
 				<input type="text" disabled id="ground-list">
 				<input type="text" disabled id="ground-scroll">
 			</div>
-			<div class="row"><b>Size</b>
+			<div class="row"><b key="themes"></b>
 				<input type="text" disabled id="size-nav">
 				<input type="text" disabled id="size-bar">
 				<input type="text" disabled id="size-list">
@@ -48,26 +45,18 @@ document.write(`
 			</div>
 		</div>
 		<div class="info">
-			<i class="icon">info</i><p class="comment">Enter any CSS3 variants (hex value, color name, or functions: hex, rbg, hsl, hsv, cmyk, linear-gradient, and so on). You will see the changes in real time, so it is recommended that you make your changes before loading the database or when there are few results.</p>	
+			<i class="icon">info</i><p key="themei" class="comment"></p>	
 		</div>
 	</blockquote>
 	-->
 
-	<b>Color scheme</b>
+	<b key="scheme"></b>
 	<blockquote>
 		<div class="table">
-			<div class="row"><b>Element</b>
-				Text
-				Ground
-				Opcode
-				Operator
-				Number
-				String
-				Variable
-				Comment
-				Label
+			<div class="row"><b key="schemee"></b>
+				<span key="schemees"></span>
 			</div>
-			<div class="row"><b>Color</b>
+			<div class="row"><b key="schemec"></b>
 				<input type="text" id="text">
 				<input type="text" id="ground">
 				<input type="text" id="opcode">
@@ -78,12 +67,12 @@ document.write(`
 				<input type="text" id="comment">
 				<input type="text" id="label">
 			</div>
-			<div class="row"><b>View</b>
+			<div class="row"><b key="schemev"></b>
 				<pre>text_example_xd\n\n00AD:\n== += -= < <= * => > - +\nfalse 10 0xFF 0.0 #FAM1\n'short' "long string"\n412@ $variable_example\n{comment} // example\n@example_0AF023</pre>
 			</div>
 		</div>
 		<div class="info">
-			<i class="icon">info</i><p class="comment">Enter any CSS3 variants (hex value, color name, or functions: hex, rbg, hsl, hsv, cmyk, linear-gradient, and so on). You will see the changes in real time, so it is recommended that you make your changes before loading the database or when there are few results.</p>	
+			<i class="icon">info</i><p key="schemei" class="comment"></p>	
 		</div>
 	</blockquote>
 
